a00aa1c26e0b534f4ceca76c4dd5d0984eb4da1e
Mai Trang <m.trang91@gmail.com>
2018-08-19 14:12:40 +0300

    update

--- test.py
+++ test.py
@@ -1,26 +1,7 @@
-MAX_LENGTH = 3
-
-def filterPair(p):
-    # if not p:
-    #     return False
-    # else:
-    #     return  len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH
-    lst = [x for x in p if len(x.split(' ')) < MAX_LENGTH ]
-    if len(lst)== 2:
-        return True
-    else:
-        return False
-
-
-def filterPairs(pairs):
-    #tmp =[pair for pair in pairs if filterPair(pair)]
-    #lst = filter(lambda pair: filterPair(pair), pairs)
-    return [pair for pair in pairs if filterPair(pair)]
-
-pairs = [['привет', 'приветос ! как тебя зовут ?'],
-[' привет', ''],
-[' ', ''],
-[' привет', 'и тебе привет !'],
-[' привет', 'привет ! кажется мы не знакомы ?'],]
-pairs = filterPairs(pairs)
-print(pairs)
+#Creat dataset
+dia =""
+with open("data/anwser_databse.txt", "a",encoding="utf-8") as text_file:
+  while dia != "exit":
+    dia = input("input text: ")
+    text_file.write(dia+"\n")
+text_file.close()
